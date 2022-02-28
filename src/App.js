9dfdb0fce10f541af5a77a08e417b5fd7e82b98f
Chintada Kiranraj <kiranrajchintada302@gmail.com>
2022-02-28 06:13:46 +0000

    IPL

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import TeamMatches from './components/TeamMatches'
+
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/team-matches/:id" component={TeamMatches} />
+    <Route exact path="/not-found" component={NotFound} />
+    <Redirect to="/not-found" />
+  </Switch>
+)
+
+export default App
---
+++ src/components/TeamCard/index.js
@@ -0,0 +1,23 @@
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const TeamCard = props => {
+  const {teamDetails} = props
+  const {name, id, teamImageURL} = teamDetails
+
+  //   const {match} = this.props
+  //   const {params} = match
+  //   const {id} = params
+
+  return (
+    <li className="team-item">
+      <Link to={`/team-matches/${id}`} className="link-item">
+        <img src={teamImageURL} alt={name} className="team-logo" />
+        <p className="team-name">{name}</p>
+      </Link>
+    </li>
+  )
+}
+
+export default TeamCard
